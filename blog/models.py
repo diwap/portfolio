9ced94cdd7d865a4b766dd61ecad23d20f596f25
diwap <dipipandey1@gmail.com>
2019-05-12 14:02:39 +0545

    value from database shown in html page

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
