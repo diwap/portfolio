0d1ff7068cefd837168a4a6e94df6c9013a9f39e
diwap <dipipandey1@gmail.com>
2019-05-14 14:15:43 +0545

    static and media url set

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,9 @@ class Blog(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
 
+    # if no default is defined, add attribute null=True, blank=True
+    cover_image = models.ImageField(upload_to="blogs", default="static/default_blog.jpg")
+    # Execute makemigrations to create migration file and migrate to populate in database
+
     def __str__(self):
         return self.title
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -117,5 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+http://localhost:8000/static/default_blog.jpg
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'static/',
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
